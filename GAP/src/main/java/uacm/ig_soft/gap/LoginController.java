c22dea796a58d9eec9d8d6f518fbe11c50094276
Luis-Aguilar-0 <luis.aguilar.castellanos@alumnos.uacm.edu.mx>
2025-03-25 15:50:27 -0600

    modificacion a la pantalla login

--- GAP/src/main/java/uacm/ig_soft/gap/LoginController.java
+++ GAP/src/main/java/uacm/ig_soft/gap/LoginController.java
@@ -8,9 +8,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 /**
  * FXML Controller class
@@ -32,10 +37,25 @@ public class LoginController implements Initializable {
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        
+        pn_login.setBackground(new Background(new BackgroundFill(Color.web("#3B2A5E"), new CornerRadii(15), Insets.EMPTY)));
+        
+        //onbtenemos los valores que se ingresan en el texfile
+        txf_Contraseña.setOnAction(event ->{
+            String correo = txf_correo.getText();
+            String contraseña = txf_Contraseña.getText();
+            System.out.println("el correo es:" + correo + "\nla contraseña es: "+ contraseña);
+        });
+       
+        
+       
+       
     }    
     
 }
