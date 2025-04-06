730483a9a9260b953a630c0a7d8eb7d3a4c1b715
Luis-Aguilar-0 <luis.aguilar.castellanos@alumnos.uacm.edu.mx>
2025-04-06 17:40:40 -0600

    se mofifico le archivo pom.xml y se creo el paquete logic y la clase Cliente

---
+++ GAP/src/main/java/logic/Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logic;
+
+/**
+ *
+ * @author arman
+ */
+public class Cliente {
+    
+    //En esta clase ira todo lo relacionado con el cliente
+    
+}
